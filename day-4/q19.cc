23169ee7f5b0402a529b7dac917f24a9d36812ec
froster02 <arushnaudiyal@gmail.com>
2021-07-04 21:55:08 +0530

    day4

--- day-4/q19.cc
+++ day-4/q19.cc
@@ -3,16 +3,27 @@
 #include <algorithm>
 using namespace std;
 
-static bool comparator(int x,int y){
-    if(__builtin_popcount(x) == __builtin_popcount(y))
-        return x < y;
-    return __builtin_popcount(x) < __builtin_popcount(y);
-}
-
 void sortByBits(vector<int>& arr) {
-    sort(arr.begin(), arr.end(), comparator);
+
+    vector<pair<int,int> > p;    
+    for(int i=0;i<arr.size();i++){
+        
+        int n=arr[i];
+        int count=0;
+        while(n!=0){
+            if(n%2==1)
+                count++;
+            n=n/2;
+        }
+        p.push_back(make_pair(count,arr[i]));   
+    }    
+    
+    sort(p.begin(),p.end());
+    for(int i=0;i<p.size();i++)
+        arr[i]=p[i].second;
+    
     for(auto x : arr)
-        cout << x << " ";
+        cout << x << endl;
 }
 
 int main(){
